18684296271a5f1742a5074740377e667623882c
Francisco Sanabria <piktonus97x@hotmail.com>
2023-03-12 01:29:16 +0000

    Making some changes to the bot

--- rropebot_bot.py
+++ rropebot_bot.py
@@ -1,7 +1,9 @@
 import asyncio
 import discord
 import os
+
 from dotenv import load_dotenv
+from discord.ext import commands
 
 # This example requires the 'message_content' intent.
 
@@ -10,24 +12,26 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-#client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!')
+# client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 #@client.event
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print('We have logged in as {}'.format(bot.user))
 
-@bot.command(name:"start", help="Starts a Pomodoro timer")
+@bot.command(name="start", help="Starts a Pomodoro timer")
 async def start_timer(ctx):
-    await
+    await ctx.send("Time to work!")
+    await asyncio.sleep(10)
+    await ctx.send("Time to play!")
 
 #@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# async def on_message(message):
+    # if message.author == client.user:
+    #     return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # if message.content.startswith('$hello'):
+    #     await message.channel.send('Hello!')
 
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
