c06c00aaf1911e395101763271af091c3601c985
Francisco Sanabria <piktonus97x@hotmail.com>
2023-03-12 02:33:21 +0000

    Understanding the await send()

--- rropebot_bot.py
+++ rropebot_bot.py
@@ -20,6 +20,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print('We have logged in as {}'.format(bot.user))
 
+
 @bot.command(name="start", help="Starts a Pomodoro timer")
 async def start_timer(ctx):
     
@@ -33,12 +34,21 @@ async def start_timer(ctx):
     # Bot is sending ending message
     start_break_em = discord.Embed(title="Time to rest!", color=0xc555ff)
     await ctx.send(embed = start_break_em) 
+    
+    """await send()
+    The await send(embed) comes from the following documentation: https://discordpy.readthedocs.io/en/stable/api.html?highlight=embed#discord.Webhook.send
+    
+     The await keyword is used to indicate that the code is asynchronous and that the program should wait for the operation to complete before moving on to the next task. ctx likely refers to the context in which the message is being sent, and send is the method being used to send the message.
+
+    The embed parameter allows you to format the message in a more visually appealing way using an embed. stop_work_em is likely a variable that stores the content of the embed.
+    """    
+
 
 @bot.command(name="stop", help="Stop a Pomodoro timer")
 async def stop_timer(ctx):
     
     # Bot is sending a first message
-    stop_work_em = discord.Embed(title="Time to start working!", color=0x7b165b)
+    stop_work_em = discord.Embed(title="Timer has been stopped!", color=0xff3c43)
     await ctx.send(embed = stop_work_em)
 
 
